22018a36596fb5acd67a53410fbaab2ee1715f01
Falk33n <tim.falk00@gmail.com>
2025-05-18 18:08:48 +0200

    feat: add sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import { LayoutHead } from '../lib/components/layout';
+	import { Sonner } from '../lib/components/ui/sonner';
 
 	let { children } = $props();
 </script>
 
 <LayoutHead />
+<Sonner />
 
 {@render children()}

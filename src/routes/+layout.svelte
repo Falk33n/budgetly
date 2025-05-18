b76649a4e9f7f69e8049d31adda416299a8cedd3
Falk33n <tim.falk00@gmail.com>
2025-05-18 17:32:50 +0200

    chore: remove types since sveltekit gives them automatically now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import { LayoutHead } from '../lib/components/layout';
-	import type { LayoutProps } from './$types';
 
-	let { children }: LayoutProps = $props();
+	let { children } = $props();
 </script>
 
 <LayoutHead />
